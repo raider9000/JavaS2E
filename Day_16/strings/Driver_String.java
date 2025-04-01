82fbb3d4db01e536019afa5be5267593af4392b6
Raider <death.raider.9000@gmail.com>
2025-04-01 15:42:33 +0530

    fincal commit of day 16 v1

--- Day_16/strings/Driver_String.java
+++ Day_16/strings/Driver_String.java
@@ -36,5 +36,26 @@ public class Driver_String {
 
         String s6 = s5 + "mellow"; //As this is concatenating with a String reference , 
         //The s5 object is created in the heap area rather than the String constant pool.
+
+        String s10 = "Helloooo";
+
+        //returns the ascii value of the character at index 0 of s10 string
+        System.out.println(s10.codePointAt(0));
+
+        //returns the character at the index 0 of s10
+        System.out.println(s10.charAt(0));
+
+        //S10 is concatenated with the value stored in the s1 String 
+        s10 = s10.concat(s1);
+
+        //returns a boolean whether the sequence given on character is contained 
+        //in s10
+        System.out.println(s10.contains("Hello"));
+
+        //the following method copies the contents of arr after converting it to a 
+        //String and then copying it to s10
+        char arr[] = {'a' , 'b'};
+        s10 = String.copyValueOf(arr); //accessed in a static way by using the classname rather than object name to get the method.
+        System.out.println(s10);
     }
 }
