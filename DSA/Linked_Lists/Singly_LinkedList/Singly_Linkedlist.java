ecdb1f2fef7139527f85f0b5bae5c5c627e460b3
Raider <death.raider.9000@gmail.com>
2025-04-07 14:28:10 +0530

    Added all the methods of a singly linked list

--- DSA/Linked_Lists/Singly_LinkedList/Singly_Linkedlist.java
+++ DSA/Linked_Lists/Singly_LinkedList/Singly_Linkedlist.java
@@ -7,21 +7,21 @@ public class Singly_Linkedlist {
     //This is done to not traverse the entire linked list everytime to add a 
     //new element 
     Node tail;
-    int size;
+    static int size;
 
     public void Insert(int data){
         //creating an object of node type and storing 10 as data through the 
         //parameterized constructor of the node class
-        Node node1 = new Node(10);
+        Node node = new Node(data);
 
         if(head  == null){
-            head = node1;
-            tail = node1;
+            head = node;
+            tail = node;
             size++;
         }
         else{
-            tail.next = node1;
-            tail = node1;
+            tail.next = node;
+            tail = node;
             size++;
         }
     }
@@ -33,8 +33,8 @@ public class Singly_Linkedlist {
         Node current = head;
 
         //if empty head will be null , so printing null
-        if(current == null){
-            System.out.println("The linked list is empty");
+        if(head == null){
+            System.err.println("The linked list is empty");
             return;
         }
 
@@ -45,4 +45,81 @@ public class Singly_Linkedlist {
         }
         System.out.println();
     }
+
+    public void update(int index, int data){
+        //if the index is out of bounds
+        if(index < 1 || index > size){
+            System.err.println("Index out of bounds");
+            return;
+        }
+        if(index == 1){
+            head.data = data;
+            return;
+        }
+        if(index == size){
+            tail.data = data;
+            return; 
+        }
+        //getting the head node to traverse the linked list upto the index using current 
+        Node current = head;
+        for(int i = 1; i <= index; i++){
+            //traversing the linked list upto the node in the given index 
+            current = current.next;
+        }
+        //updating the int value in that node 
+        current.data = data;
+    }
+
+    //simple method to insert a node at a given index
+    public void insert_at_Index(int index , int data){
+        if(index < 1 || index > size + 1){
+            System.err.println("Index out of bounds");
+            return;
+        }
+        else{
+            Node current = head;
+            for(int i = 1 ; i < index ; i++){
+                current = current.next;
+            }
+            Node node = new Node(data);
+            node.next = current.next; 
+            current.next = node;
+        }
+    }
+
+    public void delete_last(){
+        if(head == null){
+            System.err.println("The linked list is empty");
+            return;
+        }
+        if(head.next == null){
+            head = null;
+            return;
+        }
+        else{
+            Node current = head;
+            while(current.next.next != null){
+                current = current.next;
+            }
+            current.next = null;
+        }
+    }
+
+    public void delete_at_Index(int index){
+        if(head == null){
+            System.err.println("The linked list is empty");
+            return;
+        }
+        else{
+            Node current = head;
+            for(int i = 1 ; i < index - 1 ; i++){
+                current = current.next;
+            }
+            Node temp = current.next;
+            current.next = temp.next;
+            temp = null;
+            size--;
+        }
+    }
+
 }
