3c4172c0c7099ba8e15a378aac5909a6fb5ffc95
Raider <death.raider.9000@gmail.com>
2025-04-21 11:46:30 +0530

    Added Stream Basics

---
+++ Day_22/Higher_Order_Functions/Simple_Example/Test.java
@@ -0,0 +1,9 @@
+package Day_22.Higher_Order_Functions.Simple_Example;
+//This is the class containing the higher order function 
+public class Test{
+    //[Note :] A higher order function is a function that takes a function as an arguement
+    //The following is the higher order function 
+    void greet(Demo d){
+        d.print();
+    }
+}
---
+++ StreamAPI/Streaming.java
@@ -0,0 +1,39 @@
+package StreamAPI;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.*;
+//This class deals with the usage of the Stream API inside the Java.Util package
+//[Note : ] Stream is an interface that can be used after impoerting it from the 
+//util package. 
+public class Streaming{
+    //[Note : ] Streams are stateless , as a result it will not alter the original collection 
+    //It will return a new Stream everytime. 
+    public static void main(String[] args) {
+        //Creating a list and using asList inside Arrays in order to store a List
+        List<Integer> l1 = Arrays.asList(1 , 2, 5, 7, 8 , 7 , 4 , 3 , 1);
+
+        //Now trying to remove the duplicates using the Stream API , rather than 
+        //writing my own logic and creating a for loop.
+        //Also , we do not want to alter the original List but still want to perform operations on the  List. 
+
+        //In order to do the above, the List has to be converted to a Stream first.
+        //Storing the l1 List as a stream inside a Stream reference 
+        Stream s = l1.stream();
+
+        //This is the method inside Stream interface<Stream API> in order to remove the duplicates. 
+        //This also returns a Stream only. 
+        //s.distinct();
+        //However , on one stream , only one function can be done and performing any other function on that stream will result in an error as that stream is closed. 
+        //So in order to iterate over that stream , we need to create another stream reference and store that stream's function in that stream. 
+        Stream s2 = s.distinct(); //after this Stream s is closed and in order to iterate over the distince Stream that we get by the distinct method 
+        //We have to iterate over the Stream s2. 
+
+
+        //forEach is a method inside the Stream APi , which can be used on the Stream type references. 
+        //The forEach method is a higher order function , that takes a function as an arguement , So we are passing a lambda expression as an arguement. 
+        //n->(S.O.P(n)) means that in the method , directly the single line logic is passed to the interface. 
+        s2.forEach(n->System.out.println(n));//Prints the distinct elements 
+
+
+    }   
+}
