e977ba7d836efc29f04bba374efa6efd3706b4d9
Raider <death.raider.9000@gmail.com>
2025-04-24 10:59:20 +0530

    Added annotations

---
+++ Annotations/AnnotationReader.java
@@ -0,0 +1,22 @@
+package Annotations;
+
+import java.lang.reflect.Method;
+
+public class AnnotationReader {
+    public static void main(String[] args) {
+    MyService service = new MyService();
+    Class<?> clazz = service.getClass();
+
+    for (Method method : clazz.getDeclaredMethods()) {
+    if (method.isAnnotationPresent(Info.class)) {
+    Info info = method.getAnnotation(Info.class);
+
+
+    System.out.println("Method: " + method.getName());
+    System.out.println(" Name: " + info.name());
+    System.out.println(" Version: " + info.version());
+
+}
+}
+    }
+}
